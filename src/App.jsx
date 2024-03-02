e6f3bb5b9a54464bf5627f3498ef1b899b5c83c6
IrynaHavryliuk <gis.80@ukr.net>
2024-03-02 01:15:45 -0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,23 @@ const ArticleList = ({ items }) => (
 
 const App = () => {
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchArticles() {
-      const response = await axios.get(
-        "https://hn.algolia.com/api/v1/search?query=react"
-      );
-      setArticles(response.data.hits);
+      try {
+        // Set loading indicator to true before the request
+        setLoading(true);
+        const response = await axios.get(
+          "https://hn.algolia.com/api/v1/search?query=react"
+        );
+        setArticles(response.data.hits);
+      } catch (error) {
+        // Handle errors here
+      } finally {
+        // Set loading indicator back to false after the request
+        setLoading(false);
+      }
     }
 
     fetchArticles();
@@ -30,6 +40,7 @@ const App = () => {
   return (
     <div>
       <h1>Latest articles</h1>
+      {loading && <p>Loading data, please wait...</p>}
       {articles.length > 0 && <ArticleList items={articles} />}
     </div>
   );
